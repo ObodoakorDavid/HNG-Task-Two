308951e4e7bc8f7d8041ef9374e5ea43f308e5d8
ObodoakorDavid <obodoakordavid@outlook.com>
2024-07-07 13:05:04 +0100

    updates db for deployment

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,21 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-    port: process.env.DB_PORT || "5433",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-  }
-);
+let db;
+
+if (process.env.NODE_ENV === "development") {
+  db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "postgres",
+      port: process.env.DB_PORT || "5432",
+    }
+  );
+} else {
+  db = new Sequelize(process.env.POSTGRES_URL);
+}
 
 export default db;
