a89c4752f222ca2296f985f0c061504d106f4208
ObodoakorDavid <obodoakordavid@outlook.com>
2024-07-08 09:40:56 +0100

    another minor change

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ const startServer = async () => {
     console.log("Authenticating database connection...");
     await db.authenticate();
     console.log(`DB Connected!`);
+    await db.sync({ force: true });
+    console.log("Tables created!");
     app.listen(port, () =>
       console.log(`Server is Live! Running on PORT: ${port}`)
     );
